923ee7525134abed06b671cca6f6b729e7ba31ca
taikulawo <qaq1362211689@gmail.com>
2024-06-19 14:12:03 +0800

    1

--- roff/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) mod config;
-pub(crate) mod listener;
-pub(crate) mod proxy;
-pub(crate) mod stream;
-pub type SmallString = smol_str::SmolStr;
-pub type SmallVec<T> = smallvec::SmallVec<[T; 16]>;
--- roff/src/stream/mod.rs
+++
@@ -1,1 +0,0 @@
-mod sni_sniffer;
